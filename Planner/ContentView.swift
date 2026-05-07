d37902a8bb32451aa4872f9d8dc6b3e2f02d72aa
Zyphyr007 <charleschen0359@gmail.com>
2026-05-07 10:45:34 +1000

    create product

---
+++ Planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Zhenqiang Chen on 7/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Zhenqiang Chen on 7/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
